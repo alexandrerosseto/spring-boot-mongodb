0ed7eeea53d96bcc86f11bc6ef9f01746b418686
alexandrerosseto <alexandrerosseto@gmail.com>
2020-05-03 12:27:43 -0300

    First commit after the project has been created

---
+++ src/test/java/com/arosseto/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arosseto.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
